6ec45b4979aeb68f6db3dcc09e4e42cad5d22987
Larissa Nunes <lari.nih@hotmail.com>
2022-06-29 23:49:58 -0400

    quase o final 2

--- src/routes/specimen.ts
+++ src/routes/specimen.ts
@@ -26,7 +26,7 @@ specimenRouter.post('/', async (req, res) => {
     specimen.cage = cage
     specimen.zookeeper = zookeeper
     
-    const savedSpecimen = await specimenCtrl.save(new Species)
+    const savedSpecimen = await specimenCtrl.save(specimen)
 
     return res.status(201).json({ specimen: savedSpecimen })
   }
